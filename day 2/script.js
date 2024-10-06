fe7f335b6c228a4b7d4c95757df86ed5523e20c3
Xijire <145289393+Xijire@users.noreply.github.com>
2024-10-06 23:42:56 +0100

    Add files via upload

---
+++ day 2/script.js
@@ -0,0 +1,56 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+document.addEventListener('DOMContentLoaded', update);// 页面加载后初始化
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update() {
+    console.log("Current active:", currentActive);
+
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    // 更新进度条的宽度
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+    console.log('Progress width:', progress.style.width);
+    // 如果是第一个，则禁用 Prev 按钮
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
+
